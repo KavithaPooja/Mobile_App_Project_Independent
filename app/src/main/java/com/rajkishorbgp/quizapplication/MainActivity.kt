43ea984ab9a84ff1199e106b0a7dcc1d6a5b6b3c
Raj Kishor <94050675+rajkishorbgp@users.noreply.github.com>
2023-08-25 19:26:16 +0530

    add multiple layout

--- app/src/main/java/com/rajkishorbgp/quizapplication/MainActivity.kt
+++ app/src/main/java/com/rajkishorbgp/quizapplication/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.rajkishorbgp.quizapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.rajkishorbgp.quizapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startQuizCard.setOnClickListener {
+            startActivity(Intent(this@MainActivity,QuizOptionActivity::class.java))
+        }
+
+        binding.rulesCard.setOnClickListener {
+            startActivity(Intent(this@MainActivity,RulesActivity::class.java))
+        }
+
+        binding.historyCard.setOnClickListener {
+            startActivity(Intent(this@MainActivity,HistoryActivity::class.java))
+        }
+
+        binding.editPasswordCard.setOnClickListener {
+            startActivity(Intent(this@MainActivity,EditPasswordActivity::class.java))
+        }
+        binding.logoutCard.setOnClickListener {
+            Toast.makeText(this@MainActivity,"Logout",Toast.LENGTH_SHORT).show()
+        }
     }
 }
---
+++ app/src/main/java/com/rajkishorbgp/quizapplication/QuizOptionActivity.kt
@@ -0,0 +1,11 @@
+package com.rajkishorbgp.quizapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuizOptionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_option)
+    }
+}
